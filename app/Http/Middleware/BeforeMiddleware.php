8ce705341b2c81277ea7172cbd2b10ebe0417df0
dingjuru <dingjuru@caihongxingqiu.com>
2018-10-11 18:28:22 +0800

    设置前置后置中间件

---
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 前置中间件.
+ * User: dingjuru
+ * Date: 2018/10/11
+ * Time: 18:19
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class BeforeMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
