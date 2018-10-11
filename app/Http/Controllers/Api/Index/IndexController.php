fc8bcac50a19609cccc39fccbd7b69ee0828a6b3
dingjuru <dingjuru@caihongxingqiu.com>
2018-10-11 10:53:56 +0800

    first commit

---
+++ app/Http/Controllers/Api/Index/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dingjuru
+ * Date: 2018/10/11
+ * Time: 10:41
+ */
+
+namespace App\Http\Controllers\Api\Index;
+
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     *
+     */
+    public function index()
+    {
+        reutrn ['index'];
+    }
+}
