64550dd29e4ab57265d9651315f9194921f97267
dingjuru <dingjuru@caihongxingqiu.com>
2018-10-15 14:25:08 +0800

    添加路由控制

--- routes/api.php
+++ routes/api.php
@@ -43,13 +43,17 @@ $api->version('v1', function ($api) {
     $api->group([
         'namespace' => 'App\Http\Controllers\Api',
     ], function($api){
+
+        // 登录
         $api->post('login', 'User\LoginController@login')->name('login');
+        // 退出登录
+        $api->get('logout', 'User\LoginController@logout')->name('logout');
+        // 刷新token
+        $api->get('refresh', 'User\LoginController@refresh')->name('refresh');
+        // me
+        $api->get('me', 'User\LoginController@me')->name('me');
 
-        $api->post('reset', 'Api\ResetPasswordController@reset')->name('reset');
 
-        $api->get('test', function(){
-
-        });
     });
 
 
